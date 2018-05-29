6e7a4f6052f0d3996ff5ed09a3518e30619d27af
Igor Chechura <igorchechura@gmail.com>
2018-05-29 10:46:51 +0300

    Remove bootstrap and redefine vars

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,6 @@ gulp.task('styles', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		// 'app/libs/smartmenus/dist/jquery.smartmenus.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
